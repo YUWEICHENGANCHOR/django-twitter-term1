ac8a6c0dc66f0087a8a9fa25687680d1c5f4f2f7
anchor810321 <anchor810321@gmail.com>
2021-08-03 16:52:38 -0400

    Initial Django Project

--- provision.sh
+++ provision.sh
@@ -56,16 +56,19 @@ PASS="admin"
 MAIL="admin@twitter.com"
 script="
 from django.contrib.auth.models import User;
+
 username = '$USER';
 password = '$PASS';
 email = '$MAIL';
+
 if not User.objects.filter(username=username).exists():
-User.objects.create_superuser(username, email, password);
-print('Superuser created.');
+    User.objects.create_superuser(username, email, password);
+    print('Superuser created.');
 else:
 print('Superuser creation skipped.');
 "
 printf "$script" | python manage.py shell
+
 
 # 如果想直接进入/vagrant路径下
 # 请输入vagrant ssh命令进入
