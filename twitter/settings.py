1d01ab7a0d7a98bec122c4279544b90657ae1e37
anchor810321 <anchor810321@gmail.com>
2021-08-03 16:49:30 -0400

    Initial Django Project

--- twitter/settings.py
+++ twitter/settings.py
@@ -37,17 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    # third party
-    'rest_framework',
-
-    # project apps
-    'accounts',
 ]
-
-REST_FRAMEWORK = {
-    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 10
-}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -93,7 +83,6 @@ DATABASES = {
         'PASSWORD': 'yourpassword'
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
