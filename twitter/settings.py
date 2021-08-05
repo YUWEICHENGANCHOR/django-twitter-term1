5467fe73e967405249dffdf198b11bb29996b5d8
anchor810321 <anchor810321@gmail.com>
2021-08-05 16:34:58 -0400

    add signup login login_status logout in view.py and serializers.py

--- twitter/settings.py
+++ twitter/settings.py
@@ -37,8 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-
+    # third party
     'rest_framework',
+
+    # project apps
+    'accounts',
 ]
 
 REST_FRAMEWORK = {
