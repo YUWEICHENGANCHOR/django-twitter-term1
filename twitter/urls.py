f3dc7b480bbe9b024264c927e7171ea5e7441f23
anchor810321 <anchor810321@gmail.com>
2021-10-18 21:15:05 -0400

    Like api create

--- twitter/urls.py
+++ twitter/urls.py
@@ -21,6 +21,7 @@ from friendships.api.views import FriendshipViewSet
 from newsfeeds.api.views import NewsFeedViewSet
 from rest_framework import routers
 from tweets.api.views import TweetViewSet
+from likes.api.views import LikeViewSet
 
 router = routers.DefaultRouter()
 router.register(r'api/users', UserViewSet)
@@ -29,6 +30,8 @@ router.register(r'api/tweets', TweetViewSet, basename='tweets')
 router.register(r'api/friendships', FriendshipViewSet, basename='friendships')
 router.register(r'api/newsfeeds', NewsFeedViewSet, basename='newsfeeds')
 router.register(r'api/comments', CommentViewSet, basename='comments')
+router.register(r'api/likes', LikeViewSet, basename='likes')
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
