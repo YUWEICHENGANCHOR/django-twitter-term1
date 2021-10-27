3e035007d2301919cc9adaa061aa236e9458039f
anchor810321 <anchor810321@gmail.com>
2021-10-26 21:13:15 -0400

    22-userprofile-model-and-admin

--- accounts/api/views.py
+++ accounts/api/views.py
@@ -101,6 +101,10 @@ class AccountViewSet(viewsets.ViewSet):
             }, status=400)
 
         user = serializer.save()
+
+        # Create UserProfile object
+        user.profile
+        
         django_login(request, user)
         return Response({
             'success': True,
