732094a20d1b3547a21698d060a8c6f70a11283a
anchor810321 <anchor810321@gmail.com>
2021-11-04 20:47:19 -0400

    24 update tweets photos and admin

--- tweets/tests.py
+++ tweets/tests.py
@@ -4,6 +4,9 @@ from tweets.models import Tweet
 from datetime import timedelta
 from utils.time_helpers import utc_now
 from testing.testcases import TestCase
+from testing.testcases import TestCase
+from tweets.constants import TweetPhotoStatus
+from tweets.models import TweetPhoto
 
 
 class TweetTests(TestCase):
@@ -26,3 +29,14 @@ class TweetTests(TestCase):
         dongxie = self.create_user('dongxie')
         self.create_like(dongxie, self.tweet)
         self.assertEqual(self.tweet.like_set.count(), 2)
+
+    def test_create_photo(self):
+        # 测试可以成功创建 photo 的数据对象
+        photo = TweetPhoto.objects.create(
+            tweet=self.tweet,
+            user=self.linghu,
+        )
+        self.assertEqual(photo.user, self.linghu)
+        self.assertEqual(photo.status, TweetPhotoStatus.PENDING)
+        self.assertEqual(self.tweet.tweetphoto_set.count(), 1)
+
