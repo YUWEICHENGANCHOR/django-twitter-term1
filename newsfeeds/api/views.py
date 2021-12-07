83f872489ff2d91af854bb9d22603e6bba142cd9
anchor810321 <anchor810321@gmail.com>
2021-12-07 17:17:59 -0500

    20211207 34-cache-newsfeed-list-in-redis

--- newsfeeds/api/views.py
+++ newsfeeds/api/views.py
@@ -3,6 +3,7 @@ from rest_framework.permissions import IsAuthenticated
 from newsfeeds.models import NewsFeed
 from newsfeeds.api.serializers import NewsFeedSerializer
 from utils.paginations import EndlessPagination
+from newsfeeds.services import NewsFeedService
 
 
 class NewsFeedViewSet(viewsets.GenericViewSet):
@@ -17,9 +18,10 @@ class NewsFeedViewSet(viewsets.GenericViewSet):
         return NewsFeed.objects.filter(user=self.request.user)
 
     def list(self, request):
-        queryset = self.paginate_queryset(self.get_queryset())
+        newsfeeds = NewsFeedService.get_cached_newsfeeds(request.user.id)
+        page = self.paginate_queryset(newsfeeds)
         serializer = NewsFeedSerializer(
-            queryset,
+            page,
             context={'request': request},
             many=True,
         )
