4226b89e944f4166dfdc38f2d4eb7597c41fd322
anchor810321 <anchor810321@gmail.com>
2022-03-25 16:35:13 -0400

    44-hbase-friendship-serializer-and-gatekeeper

--- friendships/services.py
+++ friendships/services.py
@@ -2,6 +2,10 @@ from friendships.models import Friendship
 from django.conf import settings
 from django.core.cache import caches
 from twitter.cache import FOLLOWINGS_PATTERN
+from gatekeeper.models import GateKeeper
+from friendships.hbase_models import HBaseFollowing, HBaseFollower
+
+import time
 
 cache = caches['testing'] if settings.TESTING else caches['default']
 
@@ -62,3 +66,29 @@ class FriendshipService(object):
     def invalidate_following_cache(cls, from_user_id):
         key = FOLLOWINGS_PATTERN.format(user_id=from_user_id)
         cache.delete(key)
+
+    @classmethod
+    def follow(cls, from_user_id, to_user_id):
+        if from_user_id == to_user_id:
+            return None
+
+        if not GateKeeper.is_switch_on('switch_friendship_to_hbase'):
+            # create data in mysql
+            return Friendship.objects.create(
+                from_user_id=from_user_id,
+                to_user_id=to_user_id,
+            )
+
+        # create data in hbase
+        now = int(time.time() * 1000000)
+        HBaseFollower.create(
+            from_user_id=from_user_id,
+            to_user_id=to_user_id,
+            created_at=now,
+        )
+        return HBaseFollowing.create(
+            from_user_id=from_user_id,
+            to_user_id=to_user_id,
+            created_at=now,
+        )
+    
